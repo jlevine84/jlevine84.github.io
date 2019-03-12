261b118dffaac135c53a90d8959782feafd6e5c9
Jeff Levine <jlevine_84@yahoo.com>
2019-03-12 14:36:38 -0400

    updates

---
+++ assets/javascript/index.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+    $('.carousel').carousel();
+
+    $('.parallax').parallax();
+
+    $('.carousel.carousel-slider').carousel({
+        fullWidth: true,
+        indicators: true
+    });
+});
